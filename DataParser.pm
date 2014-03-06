9232a672c277eea18518efab7b56f7f24cf75adb
Jason Rhinelander <jason@imaginary.ca>
2014-03-06 13:38:19 -0500

    Don't bother trimming the data values
    fracdist-data.c is only used to build the .so, which contains machine
    representation of all this data anyway: reducing the size of the .c is
    thus pointless.  On top of that, it really saves very little (around
    26KB in a 2.76MB file).

--- DataParser.pm
+++ DataParser.pm
@@ -19,88 +19,6 @@ our @PVALUES;
 our $NUM_Q = -1;
 our $LINE_WRAP = 140;
 
-# Compact the C representation of the double as much as possible
-sub dreduce {
-    my $d = shift;
-
-    my $alt1 = dreduce_chop($d);
-    my $alt2 = dreduce_sprintfg($d);
-
-    # Use whichever format is shorter (prefer the first if equal)
-    return length($alt1) <= length($alt2) ? $alt1 : $alt2;
-}
-
-# Alternative one: chop off leading and (post-decimal) trailing 0s, but also screw around with any
-# possible exponent reductions if an exponent is present.  (This is slightly more involved than
-# described: we also worry about leading signs, 
-# Two non-trivialities:
-# - if it starts with a -, trim the leading zeros *after* the -
-# - if it contains an e or E, trim the numbers before the e (not at the end)
-#
-# (NB: for numbers with an exponent, only 0s
-# before the "e" get removed)
-sub dreduce_chop {
-    my $alt1 = shift;
-    $alt1 =~ s/^\+//; # Removing superfluous leading +
-    $alt1 =~ s/^(-?)0+/$1/; # Remove leading 0s (preserving a leading -, if present)
-    
-    # If the string is now empty (except for a decimal point and/or leading -), we have a zero
-    # value, so just return it.
-    if ($alt1 =~ /^(-?)\.?(?:[eE]|$)/) {
-        return "${1}0";
-    }
-
-    my ($mantissa, $e_or_E, $exponent) = split /([eE])/, $alt1, 2;
-
-    # Strip any post-decimal-point, trailing 0s from the mantissa
-    $mantissa =~ s/0+$// if $mantissa =~ /\./;
-
-    # A trailing . isn't needed
-    $mantissa =~ s/\.$//;
-
-    if (defined $exponent) {
-        # Number is in scientific notation; let's try to shrink the exponent
-        $exponent =~ s/^\+//; # Leading + is superfluous
-        $exponent =~ s/^(-?)0+/$1/; # Leading 0s superfluous (but preserve leading -)
-
-        # If all we have left is an empty exponent (or just a minus sign) we don't need an exponent
-        # at all (the exponent was 0).
-        $exponent = undef if $exponent =~ /^-?$/;
-    }
-
-    if (defined $exponent) {
-        $alt1 = join $e_or_E, $mantissa, $exponent;
-    }
-    else {
-        $alt1 = $mantissa;
-    }
-    return $alt1;
-}
-
-# Figure out how many digits of precision we need by stripping the number down to only its
-# significant digits, then pass this through sprintf with a %.Xg format (where X is the number of
-# significant digits detected).  The value then gets passed to dreduce_chop to chop it down some
-# more (if possible).
-sub dreduce_sprintfg {
-    my $d = shift;
-
-    my $signif = $d =~ s/^-//r;
-    # Remove leading 0s
-    $signif =~ s/^0+//;
-    # Remove the decimal point
-    $signif =~ s/\.//;
-    # Remove an exponent (it only scales, but doesn't change significant digits)
-    $signif =~ s/[eE][+-]?\d+$//;
-    # Removing trailing 0s (they add nothing to the number of significant digits, even if before the decimal point)
-    $signif =~ s/0+$//;
-    # Anything left is significant (could be 0, but that should only happen for exactly 0)
-    my $prec = length($signif);
-
-    # Use sprintf %g to format it with the necessary precision, then send it off to dreduce_chop to
-    # do its magic
-    return dreduce_chop(sprintf("%.${prec}g", $d));
-}
-
 # Reads $filebase01 through $filebase$numfiles, parses the values, returns them in a big string.
 sub parse_files {
     my ($filebase, $numfiles) = @_;
@@ -155,7 +73,7 @@ sub parse_files {
                         }
                     }
                     ++$pvaluepos;
-                    my $v = dreduce($2);
+                    my $v = $2;
                     # Wrap the line first if adding this one will make it too long (minus 9: 8 for the
                     # two leading tabs, 1 for the comma).
                     if (length($out) and length($out) + length($v) > $LINE_WRAP - 9) {
